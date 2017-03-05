1c17ad27e28eae304b7dd5b09ea7416dcc038c84
Simon Mirco <simon@box.id.au>
2017-03-04 18:18:22 -0800

    Moved credit to correct file

--- zerotier.py
+++ zerotier.py
@@ -1,3 +1,7 @@
+#
+# adapted from github.com/Red5d/ztadmin
+#
+
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
--- ztr53.py
+++ ztr53.py
@@ -1,7 +1,3 @@
-#
-# adapted from github.com/Red5d/ztadmin
-#
-
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
